45b9b328e2622537158a74bc50c8bd19a4cc9dff
Samer ElMehalawy <selmehalawy5001@email.vccs.edu>
2022-03-04 08:52:39 -0500

    adding the file Assignment3List1

---
+++ Assignment3List1.sh
@@ -0,0 +1,43 @@
+Mens Hairstyles 	Price	# of times Purchased last week 
+Dreadlocks             	200	                2
+Crew Cut	        40                 	4
+Bowl             	30               	4
+Flattop	                40               	5
+Quiff	                40               	8
+Mid-Fade         	30	                3
+Tousled Undercut	30	                2
+Modern Pompadour	100	                5
+Drop Fade	        50	                8
+Fringe 	                50               	3
+Caesar Cut 	        50               	3
+Short Crop 	        50               	2
+Side-Parted	        50               	2
+Bald 	                20               	5
+Buzz	                20              	7
+hard part	        50              	5
+brushed up	        80	                5
+French Crop	        100              	2
+Slicked back 	        80	                4
+
+Women's Hairstyles 	Price 	# of times Purchased last week 
+bouffant	        250              	2
+Pixie	                250              	5
+Dreadlocks 	        400	                5
+Bowl 	                100              	5
+Bob	                100                	2
+Feathered	        200	                2
+center-parted	        200             	4
+Lob	                200	                4
+Medium Cut	        200               	4
+extra long bob	        250               	2
+vcut	                250	                7
+Comb-over Lob	        200	                7
+Balayage Lob	        250                	5
+curls	                400                 	5
+relaxed braids 	        400	                5
+Braid with beads 	450               	2
+Braided Bun 	        350               	3
+simple updo	        300              	6
+Long Braids        	500	                2
+
+
